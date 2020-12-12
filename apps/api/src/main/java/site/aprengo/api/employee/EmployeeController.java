c2383596399fcbe294156b63c1de18b1c7276eea
Jan <jan.bogoryja@gmail.com>
2020-12-12 17:03:02 +0100

    Add missing autowire in EmployeeController

--- apps/api/src/main/java/site/aprengo/api/employee/EmployeeController.java
+++ apps/api/src/main/java/site/aprengo/api/employee/EmployeeController.java
@@ -1,5 +1,6 @@
 package site.aprengo.api.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import site.aprengo.api.exceptionhandling.ResourceNotFoundException;
@@ -9,7 +10,7 @@ import java.util.List;
 @RestController
 public class EmployeeController
 {
-	
+    @Autowired
     private EmployeeRepo employeeRepo;
 
     @GetMapping("/employees")
