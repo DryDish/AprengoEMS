06fb0234c912a4d8107e1fd8f884af3e195bb0d6
Teodor Jonasson <teodor_jonasson@hotmail.com>
2020-12-15 09:38:42 +0100

    Add stying to the employeeList

--- apps/ems/src/components/employees/employeeList.component.js
+++ apps/ems/src/components/employees/employeeList.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import httpService from "../../services/httpService";
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class EmployeeList extends React.Component
 {
@@ -43,20 +44,37 @@ export default class EmployeeList extends React.Component
         const employeeList = this.state.employees.map((employee, index) =>
         {
             return (
-                <li key={index}>
-                    <div>
-                        <p>Employee ID : {employee.id}</p>
-                        <p>{`Name : ${employee.firstName} ${employee.lastName}`}</p>
-                        <p>Phone Number : {employee.phoneNumber}</p>
-                        <p>Worked hours : {convertMinutesToHours(employee.minutesWorked)}</p>
-                    </div>
-                </li>
+                // <li key={index}>   Sorry cris
+                //     <div>
+                //         <p>Employee ID : {employee.id}</p>
+                //         <p>{`Name : ${employee.firstName} ${employee.lastName}`}</p>
+                //         <p>Phone Number : {employee.phoneNumber}</p>
+                //         <p>Worked hours : {convertMinutesToHours(employee.minutesWorked)}</p>
+                //     </div>
+                // </li>
+                <Container>
+                    <Row key={index}>
+                        <Col xs={1}>{employee.id}</Col>
+                        <Col>{employee.firstName} {employee.lastName}</Col>
+                        <Col>{employee.phoneNumber}</Col>
+                        <Col xs={2}>{convertMinutesToHours(employee.minutesWorked)}</Col>
+                    </Row>
+                </Container>
             );
         });
 
         return (
-            <div className="col-md-6">
-                <h4>Employee List</h4>
+            <div className="col-" style={{width: "80%", margin: "auto"}}>
+                <h3 style={{fontWeight:"bold"}}>Employee List</h3><br/>
+                <Container>
+                    <Row style={{fontWeight: "Bold"}}>
+                        <Col xs={1}>ID</Col>
+                        <Col>Full Name</Col>
+                        <Col>Phone Number</Col>
+                        <Col xs={2}>Hours</Col>
+                    </Row>
+                </Container>
+                <hr></hr>
                 <ul className="list-group">
                     {employeeList}
                 </ul>
